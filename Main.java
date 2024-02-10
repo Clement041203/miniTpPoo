025ed17798e53b74316d5a04f1f58c1daae207e7
Clement041203 <97532765+Clement041203@users.noreply.github.com>
2024-02-10 09:28:43 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,17 @@
-// Classe principale pour tester le fonctionnement du design pattern
 public class Main {
     public static void main(String[] args) {
-        // Création des cellules
-        Cell emptyCell1 = new EmptyCell();
-        Cell emptyCell2 = new EmptyCell();
-        Cell occupiedCell = new OccupiedCell();
-        
-        // Configuration de la chaîne de responsabilité
-        emptyCell1.setNext(emptyCell2);
-        emptyCell2.setNext(occupiedCell);
-        
-        // Test de la chaîne
-        Item fruit = new Item("Fruit");
-        Item dot = new Item("Dot");
-        Item ghost = new Item("Ghost");
-        Item pacman = new Item("Pacman");
+        Pacman pacman = new Pacman();
+        Ghost shadow = new Ghost("Shadow");
+        Ghost speedy = new Ghost("Speedy");
+        Ghost bashful = new Ghost("Bashful");
+        Ghost pokey = new Ghost("Pokey");
 
-        emptyCell1.handleRequest(fruit);
-        emptyCell1.handleRequest(dot);
-        emptyCell1.handleRequest(ghost);
-        emptyCell1.handleRequest(pacman);
+        pacman.addObserver(shadow);
+        pacman.addObserver(speedy);
+        pacman.addObserver(bashful);
+        pacman.addObserver(pokey);
+
+        pacman.moveTo(Direction.North);
+        pacman.moveTo(Direction.East);
     }
 }
